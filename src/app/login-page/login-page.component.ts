8e976682cee7462e4cf2200ad88e4813354e9f3c
younesRekik15 <younesrekik347@gmail.com>
2024-06-20 15:19:39 +0100

    added two inputs using @for
    also removed the background colors because it started to be messy and confusing,
    and used outline instead for the output readability (it will be removed after finiching the work)

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
 
+
+export class LoginPageComponent {
+  inputs: Input[] = [
+    {
+      id: 1,
+      name: "Email",
+      type: "email",
+      placeholder: "Example@email.com"
+    },
+    {
+      id: 2,
+      name: "Password",
+      type: "password",
+      placeholder: "At least 8 characters"
+    }
+  ];
 }
+
+export class Input {
+  "id": number;
+  "name": string;
+  "type": string;
+  "placeholder": string;
+}
+
