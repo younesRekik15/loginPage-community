7d6f744fad2fb8b85aedd6943380eb5ac847eb0f
younesRekik15 <younesrekik347@gmail.com>
2024-06-21 12:10:23 +0100

    changed the form part to be a new separatet component
    making it new component made the code less long but more readability.
    
    fixed the sign-in button value alignment to be ajusted in the middle vertically

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginPageComponent } from './login-page.component';
+import { FormComponent } from '../form/form.component';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,29 +1,16 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ FormComponent ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 
 
 export class LoginPageComponent {
-  inputs: Input[] = [
-    {
-      id: 1,
-      name: "Email",
-      type: "email",
-      placeholder: "Example@email.com"
-    },
-    {
-      id: 2,
-      name: "Password",
-      type: "password",
-      placeholder: "At least 8 characters"
-    }
-  ];
   socialButtons: SocialButton[] = [
     {
       id: 1,
@@ -34,13 +21,6 @@ export class LoginPageComponent {
       name: "Facebook"
     }
   ];
-}
-
-export class Input {
-  "id": number;
-  "name": string;
-  "type": string;
-  "placeholder": string;
 }
 
 export class SocialButton {
